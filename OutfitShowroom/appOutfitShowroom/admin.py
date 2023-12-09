06c072381bdada3b0068bbfdefe48b48fce8e55e
jorgealcorta <jorgealcorta@opendeusto.es>
2023-12-09 21:50:12 +0100

    EstiloAdmin y OcasionAdmin

--- OutfitShowroom/appOutfitShowroom/admin.py
+++ OutfitShowroom/appOutfitShowroom/admin.py
@@ -1,11 +1,21 @@
 from django.contrib import admin
 from .models import Estilo, Ocasion, Outfit
 
+class EstiloAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'nombre_en')
+    search_fields = ('nombre', 'nombre_en')
+
+class OcasionAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'nombre_en')
+    search_fields = ('nombre', 'nombre_en')
+
 class OutfitAdmin(admin.ModelAdmin):
     list_display = ('nombre', 'fecha', 'descripcion', 'precio', 'imagen')
-    search_fields = ('nombre',)
+    search_fields = ('nombre', 'descripcion')
     list_filter = ('estilos', 'ocasion')
 
-admin.site.register(Estilo)
-admin.site.register(Ocasion)
+
+
+admin.site.register(Estilo, EstiloAdmin)
+admin.site.register(Ocasion, OcasionAdmin)
 admin.site.register(Outfit, OutfitAdmin)
