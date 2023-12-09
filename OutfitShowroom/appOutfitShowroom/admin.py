1834bfc92e40a36e8903206fc175e2b03bad5874
jorgealcorta <jorgealcorta@opendeusto.es>
2023-12-09 21:37:59 +0100

    OutfitAdmin buscar por nombre y filtrar por ocasion y estilo

--- OutfitShowroom/appOutfitShowroom/admin.py
+++ OutfitShowroom/appOutfitShowroom/admin.py
@@ -2,7 +2,9 @@ from django.contrib import admin
 from .models import Estilo, Ocasion, Outfit
 
 class OutfitAdmin(admin.ModelAdmin):
-    list_display = ('nombre', 'descripcion')
+    list_display = ('nombre', 'fecha', 'descripcion', 'precio', 'imagen')
+    search_fields = ('nombre',)
+    list_filter = ('estilos', 'ocasion')
 
 admin.site.register(Estilo)
 admin.site.register(Ocasion)
