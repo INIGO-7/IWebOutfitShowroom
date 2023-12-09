1f1c3be652d7ed56937235e7853ad9d7176a0198
jorgealcorta <jorgealcorta@opendeusto.es>
2023-12-09 21:22:45 +0100

    Comienzos customizacion Admin
    Ahora Outfit muestra nombre y descripcion, ahora toca customizar todo

--- OutfitShowroom/appOutfitShowroom/admin.py
+++ OutfitShowroom/appOutfitShowroom/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from .models import Estilo, Ocasion, Outfit
 
+class OutfitAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'descripcion')
+
 admin.site.register(Estilo)
 admin.site.register(Ocasion)
-admin.site.register(Outfit)
+admin.site.register(Outfit, OutfitAdmin)
